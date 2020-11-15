169b0a59234747e46e717210a6c82591729c5696
TNNiedner <nevesth@gmail.com>
2020-11-15 16:26:13 -0300

    Department entity class

--- src/com/tnn/Main.java
+++
@@ -1,8 +0,0 @@
-package com.tnn;
-
-public class Main {
-
-    public static void main(String[] args) {
-	// write your code here
-    }
-}
---
+++ src/com/tnn/Program.java
@@ -0,0 +1,13 @@
+package com.tnn;
+
+import model.entities.Department;
+
+public class Program {
+
+    public static void main(String[] args) {
+
+        var obj = new Department(1, "Books");
+        System.out.println(obj);
+
+    }
+}
