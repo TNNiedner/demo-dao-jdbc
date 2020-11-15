20ca4101c05deb98d08a1e3488134d61734df481
TNNiedner <nevesth@gmail.com>
2020-11-15 18:23:52 -0300

    SellerDaoJDBC and DaoFactory

--- src/com/tnn/Program.java
+++ src/com/tnn/Program.java
@@ -1,5 +1,6 @@
 package com.tnn;
 
+import model.dao.DaoFactory;
 import model.entities.*;
 
 import java.util.Date;
@@ -11,6 +12,9 @@ public class Program {
         var obj = new Department(1, "Books");
 
         var seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+
+        var sellerDao = DaoFactory.createSellerDao();
+
         System.out.println(seller);
 
 
